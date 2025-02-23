87d53a0e89f60203548f2309ad4a2c970ec12f15
Shohel Rana <gsshohel1314@gmail.com>
2025-02-23 22:39:46 +0600

    Dashboard page setup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,10 @@
 @section('title', 'Dashboard')
 
 @section('content')
-<div class="w-full bg-white p-6 rounded-lg shadow-lg text-center">
-    <h2 class="text-xl font-bold">Dashboard Overview</h2>
-    <p class="mt-2 text-gray-600">Welcome to your dashboard. Here you can see an overview of your activities.</p>
+<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <div class="w-full bg-white p-6 rounded-lg shadow-lg text-center">
+        <h2 class="text-xl font-bold">Dashboard Overview</h2>
+        <p class="mt-2 text-gray-600">Welcome to your dashboard. Here you can see an overview of your activities.</p>
+    </div>
 </div>
 @endsection
